0795e1e46bd56427db74fc572944d6b92c38ffdf
Philippe Morfin <morfinphilippe@gmail.com>
2018-11-13 16:53:24 +0100

    Solved

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/tasks', to: 'tasks#index'
+  # get '/tasks/new', to: 'tasks#new'
+  # get '/tasks/:id', to: 'tasks#show', as: :task_show
+  # post '/tasks', to: 'tasks#create', as: :task
+  # patch '/tasks/:id', to: 'tasks#update'
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+
+  resources :tasks
 end
